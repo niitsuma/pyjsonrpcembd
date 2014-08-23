35d567ab9d41f964025ee573feb545eef7820e1b
Hirotaka Niitsuma <niitsuma@mub.biglobe.ne.jp>
2014-08-23 11:07:01 +0900

    cmake python3

--- example/example-c.c
+++ example/example-c.c
@@ -5,18 +5,37 @@ int main()
 {
     pyjsonrpcembdinit();
 
-    PyRun_SimpleString("print(range(2,6))");
-    printf(pyjsonrpcembdstreval("range(2,6)"));//[2, 3, 4, 5]
+    PyRun_SimpleString("print(sorted([5,2,4,3]))");
+
+
+    printf(pyjsonrpcembdstreval("sorted([5,2,4,3])"));//[2, 3, 4, 5]
     printf("\n");
-    printf(pyjsonrpcembdhandle("{\"method\": \"range\", \"params\": [2,6]}")); //[2, 3, 4, 5]
+    printf(pyjsonrpcembdhandle("{\"method\": \"sorted\", \"params\": [[5,2,4,3]]}")); //[2, 3, 4, 5]
     printf("\n");
     printf(pyjsonrpcembdstreval("_"));//[2, 3, 4, 5]
     printf("\n");
-    printf(pyjsonrpcembdhandle("{\"method\": \"min\", \"params\": [[9,3,7]]}"));
+
+    printf(pyjsonrpcembdstreval("round(1.2345,2)"));//1.23
     printf("\n");
+    printf(pyjsonrpcembdhandle("{\"method\": \"round\", \"params\": [1.2345,2]}")); //1.23
+    printf("\n");
+
+
+    printf(pyjsonrpcembdhandle("{\"method\": \"min\", \"params\": [[9,3,7]]}"));//3
+    printf("\n");
+
+    printf(pyjsonrpcembdstreval("3*4")); //12
+    printf("\n");
+    printf(pyjsonrpcembdstreval("str(12)")); //"12"
+    printf("\n");
+    printf(pyjsonrpcembdhandle("{\"method\": \"str\", \"params\": [12]}")); //"12"
+    printf("\n");
+
+
     pyjsonrpcembdstrexec("x=[2,6]");
     printf(pyjsonrpcembdstreval("x*2")); //[2, 6, 2, 6]
     printf("\n");
+
     pyjsonrpcembdstrexec("import sys");
     pyjsonrpcembdstrexec         ("setattr(sys.modules[__name__], \"y\", 323)");
     //printf(pyjsonrpcembdstreval("setattr(sys.modules[__name__], \"y\", 323)"));
@@ -24,12 +43,15 @@ int main()
     printf("\n");
     //pyjsonrpcembdstrexec("z=_"); //not implemented 
 
+
     pyjsonrpcembdstrexec("import numpy");
-    printf(pyjsonrpcembdstreval("numpy.linalg.norm([1,2])"));
+    printf(pyjsonrpcembdstreval("numpy.linalg.norm([1,2])")); //2.2360679774997898
     printf("\n");
     printf(pyjsonrpcembdhandle("{\"method\": \"numpy.linalg.norm\", \"params\": [[1,2]]}")); 
     printf("\n");
 
-    Py_Finalize();
+    
+    //Py_Finalize(); // bug python3 http://stackoverflow.com/questions/8798905/does-the-python-3-interpreter-leak-memory-when-embedded
+    //work with python2.7
     return 0;
 }
--- example/example-racket.rkt
+++ example/example-racket.rkt
@@ -1,5 +1,10 @@
 #lang racket
 
+; usage this file
+; 1. install pyjsonrpcembd
+; 2. locate  example-racket.rkt  python-on-racket.rkt same directory
+; 3. $ racket example-racket.rkt
+
 (require "python-on-racket.rkt")
 (require json)
 ;;;=================init end====================
@@ -10,35 +15,35 @@
 (pystr "str(3*4)") ;;=>"12"
 (pystrexec "print(3*4)")
 
+(pystr "sorted([5,2,4,3])") ;; =>'(2 3 4 5)
+(py "sorted" '((5 2 4 3)))  ;; =>'(2 3 4 5)
+(pyjson "{\"method\": \"sorted\", \"params\": [[5,2,4,3]]}")
+(pyjson (method-params->json-str "sorted" '((5 2 4 3))))
+
 (pystrexec "import numpy")
 (pystr "numpy.linalg.norm([1,2])") ;;=> 2.2360679774997898
 (py "numpy.linalg.norm"  '((1 2))) ;;=> 2.2360679774997898
-
-(pystrexec "print(numpy.array([3,4]))")
-
 
 (pystrexec "x=[3,4]")
 (pystr "x*2") ;;=> '(3 4 3 4)
 (pystrexec (format "x=json.loads('~a')" (jsexpr->string '(3 4))))
 (pystr "x*2") ;;=> '(3 4 3 4)
 
-
-(pystr "range(2,6)") ;; =>'(2 3 4 5)
-(py "range" '(2 6))  ;; =>'(2 3 4 5)
-(pystr (format "apply(range,json.loads('~a'))" (jsexpr->string '(2 6)))) ;;=> '(2 3 4 5)
-(pyjson (method-params->json-str "range" '(2 6))) ;;=> '(2 3 4 5)
-(pyjson "{\"method\": \"range\", \"params\": [2,6]}")  ;; =>'(2 3 4 5)
+(pystr "round(1.2345,2)");;=>1.23
+(py "round" '(1.2345 2));;=>1.23
+(pystr (format "apply(round,json.loads('~a'))" (jsexpr->string '(1.2345 2)))) ;;=>1.23
+(pyjson (method-params->json-str "round" '(1.2345 2))) ;;=>1.23
+(pyjson "{\"method\": \"round\", \"params\": [1.2345,2]}")  ;;=>1.23
 
 (string->jsexpr 
  (pyjson->json 
   (jsexpr->string 
    (make-hash
-    '((method . "range") ( params . (2 6))))))) ;;=> '(2 3 4 5)
+    '((method . "round") ( params . (1.2345 2))))))) ;;=>1.23
 
-(pystr "_") ;;=> '(2 3 4 5) ;; _ = last_value
-
-
-(pyjson "{\"method\": \"min\", \"params\": [[9,3,7]]}") ;;=> 3 
+(pystr "_") ;;=>1.23 ;; _ = last_value
 
 
-(pyfinalize)
+;(pyfinalize)
+;;; bug python3 http://stackoverflow.com/questions/8798905/does-the-python-3-interpreter-leak-memory-when-embedded
+;;; but pyfinalize works with python2.7
